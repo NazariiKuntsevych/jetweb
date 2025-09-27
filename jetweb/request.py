631a866919beefa4c917bedc6b096696ceb04bc8
Nazarii Kuntsevych <kuntsevych.n.v@gmail.com>
2025-09-27 11:28:16 +0300

    feat: add utils for parsing request data

--- jetweb/request.py
+++ jetweb/request.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from json import loads
+from urllib.parse import parse_qsl
 
 from .datastructures import CaseInsensitiveDict
 
@@ -22,3 +23,35 @@ class Request:
             raise ValueError("Content type must be application/json")
 
         return loads(self.body)
+
+
+def parse_query_params(environ: dict) -> CaseInsensitiveDict:
+    return CaseInsensitiveDict(parse_qsl(environ["QUERY_STRING"], True))
+
+
+def parse_headers(environ: dict) -> CaseInsensitiveDict:
+    filtered_headers = filter(
+        lambda item: item[0].startswith("HTTP_") or item[0] in ("CONTENT_TYPE", "CONTENT_LENGTH"),
+        environ.items(),
+    )
+    formatted_headers = map(
+        lambda item: ("-".join(item[0].replace("HTTP_", "").split("_")), item[1]),
+        filtered_headers,
+    )
+    return CaseInsensitiveDict(formatted_headers)
+
+
+def parse_body(environ: dict) -> bytes:
+    content_length = int(environ.get("CONTENT_LENGTH") or 0)
+    readable = environ["wsgi.input"]
+    return readable.read(content_length)
+
+
+def make_request(environ: dict) -> Request:
+    return Request(
+        method=environ["REQUEST_METHOD"],
+        endpoint=environ["PATH_INFO"],
+        query_params=parse_query_params(environ),
+        headers=parse_headers(environ),
+        body=parse_body(environ),
+    )
